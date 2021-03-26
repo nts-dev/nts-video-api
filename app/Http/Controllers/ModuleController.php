827901719aa7ddc89e490197919c5b26df8e74a1
kennanObura <kennan.obura2@gmail.com>
2021-03-26 12:56:41 +0300

    removed pagination

--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -14,7 +14,7 @@ class ModuleController extends Controller
 
     public function __construct()
     {
-        // $this->middleware('auth:api');
+         $this->middleware('auth:api');
     }
 
     /**
