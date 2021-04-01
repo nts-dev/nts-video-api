d05d5767b5904fb9c40383dcb4d42759f3c60c74
nts-dev <daniel@nts.nl>
2021-04-01 15:12:44 +0300

    Fixed bugs on UploadController for fetch all

--- vendor/symfony/http-foundation/Request.php
+++ vendor/symfony/http-foundation/Request.php
@@ -853,7 +853,7 @@ class Request
     /**
      * Returns the root path from which this request is executed.
      *
-     * Suppose that an index.php file instantiates this request object:
+     * Suppose that an auth.php file instantiates this request object:
      *
      *  * http://localhost/index.php         returns an empty string
      *  * http://localhost/index.php/page    returns an empty string
@@ -877,7 +877,7 @@ class Request
      * The base URL never ends with a /.
      *
      * This is similar to getBasePath(), except that it also includes the
-     * script filename (e.g. index.php) if one exists.
+     * script filename (e.g. auth.php) if one exists.
      *
      * @return string The raw URL (i.e. not urldecoded)
      */
