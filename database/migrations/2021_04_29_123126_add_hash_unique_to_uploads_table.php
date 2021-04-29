ba87321bacccc628cd734d3b7e360fe8fafb7f25
nts-dev <daniel@nts.nl>
2021-04-29 15:42:09 +0300

    Hash migration rollback

--- database/migrations/2021_04_29_123126_add_hash_unique_to_uploads_table.php
+++ database/migrations/2021_04_29_123126_add_hash_unique_to_uploads_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddHashToUploadsTable extends Migration
+class AddHashUniqueToUploadsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,8 @@ class AddHashToUploadsTable extends Migration
     public function up()
     {
         Schema::table('uploads', function (Blueprint $table) {
-            $table->string('hash')->nullable()->unique()->change();
+            $table->string('hash')->unique();
+            $table->index(['hash']);
         });
     }
 
