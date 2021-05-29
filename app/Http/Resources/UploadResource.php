6c70df070a13c95cd6e7fb1c276f328b41a38d46
nts-dev <daniel@nts.nl>
2021-05-29 12:31:36 +0300

    Upload paths

--- app/Http/Resources/UploadResource.php
+++ app/Http/Resources/UploadResource.php
@@ -20,26 +20,26 @@ class UploadResource extends JsonResource
         $URL = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
         $ROOT = !$this->isUriLocalOrNTS($URL)
             ? '/nts-programs/nts-video-api/storage/app/'
-            : '/storage/app/';
+            : '/storage/';
 
         return [
             'id' => $this->id,
             'hash' => $this->hash,
             'title' => $this->title,
             'description' => $this->description,
-            'thumbnailLink' => $ROOT . $this->disk . "/thumbnails/pic" . rand(0, 3) . ".png",
-            'sprintLink' => $ROOT . $this->disk . "/sprint/index.jpg",
+            'thumbnailLink' =>$URL . $ROOT . $this->disk . "/thumbnails/pic" . rand(0, 3) . ".png",
+            'sprintLink' => $URL . $ROOT .  "/sprint/index.jpg",
             'videoLink_raw' => $URL . $ROOT . $this->raw_link,
             'disk' => $this->disk,
-            'webm' => $ROOT . $this->disk . "/web.webm",
-            'asInDisk' => $this->raw_link,
-            'videoLink' => $ROOT . $this->disk . "/hsl/master.m3u8",
+            'webm' =>$URL . $ROOT . $this->disk . "/web.webm",
+            'asInDisk' =>  $this->raw_link,
+            'videoLink' =>$URL . $ROOT . $this->disk . "/hsl/master.m3u8",
             'publish' => $this->publish,
             'created_at' => (string)$this->created_at,
             'updated_at' => (string)$this->updated_at,
 //            'user_first_name' => isset($this->user->profile) ? $this->user->profile->first_name : null,
 //            'user_last_name' => isset($this->user->profile) ? $this->user->profile->last_name : null,
-//            'user_picture' => isset($this->user->profile) ? $this->user->profile->profile_pic_url : null,
+//            'user_picture' => isset($this->user->profile) ? $this->us er->profile->profile_pic_url : null,
             'total_views' => isset($this->views) ? $this->views->count() : 0,
             'subtitles' => $this->subtitle,
             'subject_id' => $this->subject_id,
